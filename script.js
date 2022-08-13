907b86cd6acc91e2d99031af88bb47465e35d8b5
szabi1112 <banki.szabolcs@gmail.com>
2022-08-13 15:14:03 +0200

    Dizájn kész, egyenlőre a Grideset is életbehagyom

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+Event propagation kéne
+rákattintok a calculatorra kell egyszer:
+a)
+    számra kattintok mit csináljon meghívja
+
+
+b) function(érték)
+input értéke =+ érték
+    */
+
+const inputResult = document.getElementById("inputResult");
+const writeToInput = function (value) {};
+
+writeToInput("helóbeló");
